1ac83466241d9187eb5b1a5cca9458b3dd644aea
yadon079 <zeidgame@gmail.com>
2022-04-11 22:13:20 +0900

    feature : 프론트 컨트롤러 V3
    <description>
    * 회원 관리 웹 앱
    - 프론트 컨트롤러 V3
    >> Model 객체 추가. 기존에는 HttpServletRequest가 model 역할.
    
    <issue-number>

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,31 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+
+  private String viewName;
+  private Map<String, Object> mdoel = new HashMap<>();
+
+  public ModelView(String viewName) {
+    this.viewName = viewName;
+  }
+
+  public String getViewName() {
+    return viewName;
+  }
+
+  public void setViewName(String viewName) {
+    this.viewName = viewName;
+  }
+
+  public Map<String, Object> getMdoel() {
+    return mdoel;
+  }
+
+  public void setMdoel(Map<String, Object> mdoel) {
+    this.mdoel = mdoel;
+  }
+
+}
